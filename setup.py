bd41e0f262a51055278be63af3df5f0a6848ff85
Matthew DeMaere <matt.demaere@gmail.com>
2020-03-24 10:10:12 +1100

    minor fix

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ def curl_exists():
     try:
         subprocess.check_call(['curl', '--version'])
         return True
-    except subprocess.CalledProcessError as e:
+    except:
         return False
 
 
@@ -71,7 +71,7 @@ def wget_exists():
     try:
         subprocess.check_call(['wget', '--version'])
         return True
-    except subprocess.CalledProcessError as e:
+    except:
         return False
 
 
