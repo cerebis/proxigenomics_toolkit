cd20c4b68923966d933e530ea10912f2585b9aa9
Matthew DeMaere <matt.demaere@gmail.com>
2024-02-08 15:46:35 +1100

    version bump 0.7-dev

--- proxigenomics_toolkit/_version.py
+++ proxigenomics_toolkit/_version.py
@@ -1,4 +1,4 @@
-__version__ = '0.6-beta'
+__version__ = '0.7-dev'
 
 __copyright__ = """Copyright (C) 2019 Matthew Z DeMaere
 This is free software.  You may redistribute copies of it under the terms of
