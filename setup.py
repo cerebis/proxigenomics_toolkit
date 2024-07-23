e6f45c3d7569e93074decef36d94c16ded41534b
Matthew DeMaere <matt.demaere@gmail.com>
2024-07-23 15:45:38 +1000

    incremented python requirement to 3.11

--- setup.py
+++ setup.py
@@ -167,7 +167,7 @@ setup(
                       ],
 
     classifiers=[
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: POSIX :: Linux',
         'Intended Audience :: Science/Research',
