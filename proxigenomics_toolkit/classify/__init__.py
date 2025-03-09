b72220adf8da197b600012dfff2a4e3547ef1eab
Matthew DeMaere <matt.demaere@gmail.com>
2025-03-10 10:50:06 +1100

    minor change

--- proxigenomics_toolkit/classify/__init__.py
+++ proxigenomics_toolkit/classify/__init__.py
@@ -2,5 +2,5 @@ from .._version import __version__
 
 from .embedding import MetagenomeEmbeddings
 from .keras_model import ContactClassifier
-from .labeller import prepare_labelled_training_data
+from .labeller import DataLabeller
 from .significance import SignificantLinks
